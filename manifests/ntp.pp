622965b5809ff891b14535d88cf765b27330712d
Brian Warsing <bcw@sfu.ca>
2014-02-21 10:44:07 -0800

    add validations for the options keys

--- manifests/ntp.pp
+++ manifests/ntp.pp
@@ -3,6 +3,11 @@
 #
 class managedmac::ntp ($options) {
 
-  validate_hash($options)
+  validate_hash  ($options)
+  validate_array ($options[servers])
+
+  unless is_integer($options[max_offset]) {
+    fail("max_offset not an Integer: ${options[max_offset]}")
+  }
 
 }
