dfa480411bde1fc241b75772cb372a4acb456062
Brian Warsing <bcw@sfu.ca>
2014-02-24 12:24:06 -0800

    cleanup lint warnings; make service require file

--- manifests/ntp.pp
+++ manifests/ntp.pp
@@ -14,14 +14,15 @@ class managedmac::ntp ($options) {
     ensure  => present,
     owner   => 'root',
     group   => 'wheel',
-    mode    => 0644,
+    mode    => '0644',
     path    => '/private/etc/ntp.conf',
     content => inline_template("<%= @options['servers'].join('\n') %>"),
   }
 
-  service { "org.ntp.ntpd":
+  service { 'org.ntp.ntpd':
     ensure  => running,
     enable  => true,
+    require => File['ntp_conf'],
   }
 
 }
