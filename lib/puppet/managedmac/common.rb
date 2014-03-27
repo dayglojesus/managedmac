97ad77e7d0c61f337d6dd9775abb4a242f7cda89
Brian Warsing <bcw@sfu.ca>
2014-03-27 12:25:50 -0700

    add ManagedMacCommon module for storing common methods

---
+++ lib/puppet/managedmac/common.rb
@@ -0,0 +1,29 @@
+module ManagedMacCommon
+  
+  # Recurse the data argument and transform it into real Ruby objects
+  def self.destringify(data)
+    case data
+    when /\A-?\d+\z/ # Fixnum
+      data.to_i
+    when /\A-?\d+\.\d+\z/ # Float
+      data.to_f
+    when /\Atrue\z/ # TrueClass
+      true
+    when /\Afalse\z/ # FalseClass
+      false
+    when NilClass
+      data.to_s
+    when String, Fixnum, Float, TrueClass, FalseClass # Leave my elevator alone
+      data
+    when Array
+      data.map { |e| destringify_content e }
+    when Hash
+      Hash[ data.map { |k, v| [k, destringify(v)] } ]
+    else
+      raise Puppet::Error, "Cast Error: #destringify unknown type: 
+        #{data.class}, #{data}"
+    end
+  end
+  
+end
+
