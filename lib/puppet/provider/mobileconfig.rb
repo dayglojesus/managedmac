78929a65bd1cc75b4e65703f668104e8a8c04ece
Brian Warsing <bcw@sfu.ca>
2014-03-27 13:55:47 -0700

    remove debug statement

--- lib/puppet/provider/mobileconfig.rb
+++ lib/puppet/provider/mobileconfig.rb
@@ -17,7 +17,6 @@ class Puppet::Provider::MobileConfig < Puppet::Provider
     
     # Puppet MAGIC
     def prefetch(resources)
-      binding.pry
       instances.each do |prov|
         if resource = resources[prov.name]
           resource.provider = prov
