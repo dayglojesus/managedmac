9e57af4c9253cb8d883ec82f908b13f78db02803
Brian Warsing <bcw@sfu.ca>
2014-03-27 08:16:54 -0700

    include Helpers module in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,12 @@ require 'bundler/setup'
 require 'rspec-puppet'
 require 'puppetlabs_spec_helper/rake_tasks'
 require 'hiera-puppet-helper'
+require_relative './helpers'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
+  c.include Helpers
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
 end
