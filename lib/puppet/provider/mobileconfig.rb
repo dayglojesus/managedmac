b0ba6a1f0f47f77b09217bdc1a267ffefee1cb72
Brian Warsing <bcw@sfu.ca>
2014-04-10 12:12:13 -0700

    restore auto-generation of the PayloadIdentifier key

--- lib/puppet/provider/mobileconfig.rb
+++ lib/puppet/provider/mobileconfig.rb
@@ -77,7 +77,8 @@ class Puppet::Provider::MobileConfig < Puppet::Provider
         settings = item.delete('PayloadContent')
         
         # We do not compare these keys
-        variable_keys = ['PayloadDescription',
+        variable_keys = ['PayloadIdentifier',
+                         'PayloadDescription',
                          'PayloadDisplayName',
                          'PayloadOrganization',
                          'PayloadRemovalDisallowed',
@@ -134,7 +135,10 @@ class Puppet::Provider::MobileConfig < Puppet::Provider
     return [] if content.empty?
     content.collect do |payload|
       embedded_payload_uuid = payload['PayloadUUID'] || SecureRandom.uuid
+      embedded_payload_id   = payload['PayloadIdentifier'] || [@resource[:name],
+                                      embedded_payload_uuid].join('.')
       payload.merge!({
+        'PayloadIdentifier' => embedded_payload_id,
         'PayloadUUID'       => embedded_payload_uuid,
         'PayloadEnabled'    => true,
         'PayloadVersion'    => 1,
