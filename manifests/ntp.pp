dccccaf9b439d8da8402115fb4105c5a5796ba06
Brian Warsing <bcw@sfu.ca>
2014-04-11 16:08:20 -0700

    re-org to accomodate ensurability - add default options hash

--- manifests/ntp.pp
+++ manifests/ntp.pp
@@ -1,41 +1,60 @@
 # Class: managedmac::ntp
 #
 #
-class managedmac::ntp ($options) {
+class managedmac::ntp ($ensure = present, $options = {}) {
 
-  validate_hash  ($options)
-  validate_array ($options[servers])
+  # Only validate varaiables if ensure=present
+  if $ensure == present {
 
-  unless is_integer($options[max_offset]) {
-    fail("max_offset not an Integer: ${options[max_offset]}")
+    validate_hash ($options)
+    # servers
+    validate_array ($options[servers])
+    # max_offset
+    unless is_integer($options[max_offset]) {
+      fail("max_offset not an Integer: ${options[max_offset]}")
+    }
+
+    $service_state = running
+    $enabled = true
+    $ntp_conf_template = inline_template("<%= (@options['servers'].collect {
+      |x| ['server', x].join('\s') }).join('\n') %>")
+
+  } else {
+
+    unless $ensure == 'absent' {
+      fail("Parameter Error: invalid value for :ensure, ${ensure}")
+    }
+
+    $service_state = stopped
+    $enabled = false
+    $ntp_conf_template = 'time.apple.com'
   }
-
-  $ntp_conf_template = "<%= (@options['servers'].collect {
-    |x| ['server', x].join('\s') }).join('\n') %>"
 
   $ntp_service_label = 'org.ntp.ntpd'
 
   file { 'ntp_conf':
-    ensure  => present,
+    ensure  => file,
     owner   => 'root',
     group   => 'wheel',
     mode    => '0644',
     path    => '/private/etc/ntp.conf',
-    content => inline_template($ntp_conf_template),
+    content => $ntp_conf_template,
     notify  => Service[$ntp_service_label],
   }
 
   service { $ntp_service_label:
-    ensure  => running,
-    enable  => true,
+    ensure  => $service_state,
+    enable  => $enabled,
     require => File['ntp_conf'],
   }
 
-  if $::ntp_offset > $options[max_offset] {
-    exec { 'ntp_sync':
-      command => "/bin/launchctl stop ${ntp_service_label}",
-      notify  => Service[$ntp_service_label],
-      require => File['ntp_conf'],
+  if $ensure == present {
+    if abs($::ntp_offset) > $options[max_offset] {
+      exec { 'ntp_sync':
+        command => "/bin/launchctl stop ${ntp_service_label}",
+        notify  => Service[$ntp_service_label],
+        require => [File['ntp_conf'], Service[$ntp_service_label]]
+      }
     }
   }
 
