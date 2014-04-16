fdd65bd546685231dbb12adc9da99e435a2ac73f
Brian Warsing <bcw@sfu.ca>
2014-04-16 14:56:19 -0700

    refactor softwareupdate to include $catalog_url parameter

--- manifests/softwareupdate.pp
+++ manifests/softwareupdate.pp
@@ -1,16 +1,33 @@
 # == Class: managedmac::softwareupdate
-class managedmac::softwareupdate ($ensure = present, $options = {}) {
+class managedmac::softwareupdate (
+
+  $ensure       = present,
+  $catalog_url  = '',
+  $options      = {}
+
+) {
+
+  $all_params = {}
 
   # Only validate required variables if we are activating the resource
   if $ensure == present {
 
     validate_hash ($options)
 
-    # CatalogURL
-    validate_string ($options[CatalogURL])
-    if empty($options[CatalogURL]) {
-      fail('Missing Option: CatalogURL')
+    unless empty($catalog_url) {
+      validate_re ($catalog_url, '\Ahttp:\/\/(\w+\.)+\w+\/')
+      $all_params[CatalogURL] = $catalog_url
     }
+
+    if empty($options) and empty($all_params) {
+      fail('Missing Options: you have specified no params and the options
+        Hash is empty')
+    }
+
+    # Merge the parameters into the options Hash
+    # - parameters trump options
+    $compiled_options = merge($options, $all_params)
+    $compiled_options[PayloadType] = 'com.apple.SoftwareUpdate'
 
   } else {
     unless $ensure == 'absent' {
@@ -18,14 +35,12 @@ class managedmac::softwareupdate ($ensure = present, $options = {}) {
     }
   }
 
-  $options[PayloadType] = 'com.apple.SoftwareUpdate'
-
   mobileconfig { 'managedmac.softwareupdate.alacarte':
     ensure       => $ensure,
     displayname  => 'Managed Mac: Software Update',
     description  => 'Software Update configuration. Installed by Puppet.',
     organization => 'Simon Fraser University',
-    content      => [$options],
+    content      => [$compiled_options],
   }
 
 }
