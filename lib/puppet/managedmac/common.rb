dc63adf02f7b582e124edb662f12ddadfeae47d7
Brian Warsing <bcw@sfu.ca>
2014-05-01 16:17:52 -0700

    fix #dscl_find_by - fixes not quoting values with whitespace - dscl cannot search for record names with backslashes so implement a workaround - return exact match in the dscl_result string there should never be dupes or multiple records returned

--- lib/puppet/managedmac/common.rb
+++ lib/puppet/managedmac/common.rb
@@ -52,12 +52,22 @@ module ManagedMacCommon
       raise Puppet::Error, "Search params empty: \'#{attribute}\', \'#{value}\'"
     end
 
-    type = record_type.to_s.capitalize
-    cmd_args = [DSCL, SEARCH_NODE, '-search',
-      "/#{record_type.to_s.capitalize}", attribute.to_s, value.to_s]
+    # If we are looking for a record name, if might have backslahes in it
+    # Active Directory records are returned this way, so split it.
+    domain, value = value.split('\\') if value =~ /\w\\\w+/ and attribute.eql? 'name'
 
+    cmd_args = [DSCL, SEARCH_NODE, '-search',
+      "/#{record_type.to_s.capitalize}", attribute.to_s, "\'#{value.to_s}\'"]
+
+    # Excute the search
     dscl_result = `#{cmd_args.join(' ')}`
-    dscl_result.scan(/^\w+/)
+
+    # If there is a domain component to the record name, rejoin them
+    if domain and attribute.eql? 'name'
+      value = [domain, value].join('\\') if domain
+    end
+
+    dscl_result.scan /#{Regexp.quote(value)}/i
   end
 
 end
