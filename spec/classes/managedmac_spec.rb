7b229364b5f91f7f49d8f13eaba2a7bfca4868fe
Brian Warsing <bcw@sfu.ca>
2014-05-05 11:18:35 -0700

    remove trailing whitespace

--- spec/classes/managedmac_spec.rb
+++ spec/classes/managedmac_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'managedmac', :type => 'class' do
-  
+
   # managedmac is for Darwin and more specfically, macosx_productversion_major >= 10.9
   context "on an unsupported operating system" do
     # setup some variables
@@ -12,50 +12,50 @@ describe 'managedmac', :type => 'class' do
       { :osfamily => random_os_family }
     end
     # Finally, test the code
-    it "should raise a Puppet:Error" do 
+    it "should raise a Puppet:Error" do
       expect { should compile }.to raise_error(Puppet::Error, /unsupported osfamily/)
     end
   end
-  
+
   context "on an unsupported product version" do
     # here we setup two fake facts:
     # yes, we are Darwin
     # no, we are not Mavericks (10.9)
     let :facts do
-      { 
+      {
         :osfamily => 'Darwin',
         :macosx_productversion_major => '10.8',
       }
     end
     # Test the Puppet fail directive
-    it "should raise a Puppet:Error" do 
+    it "should raise a Puppet:Error" do
       expect { should compile }.to raise_error(Puppet::Error, /unsupported product version/)
     end
   end
-  
+
   # The remainder of our specs will go inside this context block
   context "on a supported operating system and product version" do
     # On our target platform, we should have green lights.
     let :facts do
-      { 
+      {
         :osfamily => 'Darwin',
         :macosx_productversion_major => '10.9',
       }
     end
-    
+
     let :hiera_data do
       {
         'managedmac::ntp::options' => options_ntp,
         'managedmac::activedirectory::options' => options_activedirectory,
       }
     end
-    
+
     context "class configuration options are set" do
       it { should contain_class('managedmac::ntp') }
       it { should contain_class('managedmac::activedirectory') }
     end
-    
+
     it { should compile.with_all_deps }
   end
-  
+
 end
