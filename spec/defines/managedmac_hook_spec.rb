860e178379b78f5f151dc0e3d052719c18841d39
Brian Warsing <bcw@sfu.ca>
2014-05-13 12:17:58 -0700

    a complete rewrite of the hook spec

--- spec/defines/managedmac_hook_spec.rb
+++ spec/defines/managedmac_hook_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 
 describe "managedmac::hook" do
 
-  let(:title) { 'loginhook' }
+
 
   context "when passed no params" do
+
+    let(:title) { 'login' }
 
     specify do
       expect {
@@ -14,88 +16,110 @@ describe "managedmac::hook" do
 
   end
 
-  context "when enable == true" do
+  context "name != 'login' or 'logout'" do
 
-    context "when type is INVALID" do
+    let(:title) { 'foo' }
 
-      let(:params) do
-        { :type => 'foo', :enable => true, :scripts => '/etc/loginhooks' }
-      end
-
-      specify do
-        expect {
-          should compile
-        }.to raise_error(Puppet::Error, /validate_re.*does not match/)
-      end
-
+    let(:params) do
+      { :enable => true, :scripts => '/etc/loginhooks' }
     end
 
-    context "when type is VALID" do
-
-      let(:params) do
-        { :type => 'login', :enable => true, :scripts => '/etc/loginhooks' }
-      end
-
-
-      specify do
-        should contain_file('/etc/loginhooks').with(
-          { 'ensure' => 'directory',
-            'owner'  => 'root',
-            'group'  => 'wheel',
-            'mode'   => '0750',
-        })
-      end
-
-      specify do
-        should contain_file('/etc/masterhooks').with(
-          { 'ensure' => 'directory',
-            'owner'  => 'root',
-            'group'  => 'wheel',
-            'mode'   => '0750',
-        })
-      end
-
-      specify do
-        should contain_file('/etc/masterhooks/loginhook.rb').with(
-          { 'ensure' => 'file',
-            'owner'  => 'root',
-            'group'  => 'wheel',
-            'mode'   => '0750',
-        })
-      end
-
-      it { should contain_exec('activate_hook') }
-
+    specify do
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /Parameter Error: invalid :name/)
     end
 
   end
 
-  context "when enable == false" do
+  context "name == 'login' or 'logout' or derivative" do
 
-    context "when type is INVALID" do
+    accepted_names = ['login', 'logout', 'LOGIN', 'LOGOUT', 'lOGiN', 'lOGOuT']
+    random_name    = accepted_names[rand(accepted_names.length - 1)]
 
+    let(:title) { random_name }
+
+    context "when enable is not a BOOL" do
       let(:params) do
-        { :type => 'foo', :enable => false, :scripts => '/etc/loginhooks' }
+        { :enable => 'foo', :scripts => '/' }
       end
-
       specify do
         expect {
           should compile
-        }.to raise_error(Puppet::Error, /validate_re.*does not match/)
+        }.to raise_error(Puppet::Error, /not a boolean/)
+      end
+    end
+
+    context "when enable == true" do
+
+      let(:params) do
+        { :enable => true }
+      end
+
+      context "when $scripts is set not set" do
+        specify do
+          expect {
+            should compile
+          }.to raise_error(Puppet::Error, /Must pass scripts/)
+        end
+      end
+
+      context "when $scripts is not an absolute path" do
+        let(:params) do
+          { :enable => true, :scripts => 'whatever' }
+        end
+        specify do
+          expect {
+            should compile
+          }.to raise_error(Puppet::Error, /not an absolute path/)
+        end
+      end
+
+      context "when $scripts is an absolute path" do
+        type = random_name.downcase
+        let(:params) do
+          { :enable => true, :scripts => "/etc/#{type}hooks" }
+        end
+        specify do
+          should contain_file("/etc/#{type}hooks").with(
+            { 'ensure' => 'directory',
+              'owner'  => 'root',
+              'group'  => 'wheel',
+              'mode'   => '0750',
+          })
+        end
+        specify do
+          should contain_file('/etc/masterhooks').with(
+            { 'ensure' => 'directory',
+              'owner'  => 'root',
+              'group'  => 'wheel',
+              'mode'   => '0750',
+          })
+        end
+        specify do
+
+          should contain_file("/etc/masterhooks/#{type}hook.rb").with(
+            { 'ensure' => 'file',
+              'owner'  => 'root',
+              'group'  => 'wheel',
+              'mode'   => '0750',
+          })
+        end
+        it { should contain_exec('activate_hook') }
       end
 
     end
 
-    context "when type is VALID" do
-
+    context "when enable == false" do
+      type = random_name.downcase
       let(:params) do
-        { :type => 'login', :enable => false, :scripts => '/etc/loginhooks' }
+        { :enable => false, :scripts => "/etc/#{type}hooks" }
       end
 
       it { should_not contain_file('/etc/masterhooks').with(
         { 'ensure' => 'absent',}) }
 
-      it { should contain_file('/etc/masterhooks/loginhook.rb').with(
+      it { should contain_file("/etc/masterhooks/#{type}hook.rb").with(
         { 'ensure' => 'absent',}) }
 
       it { should contain_exec('deactivate_hook') }
