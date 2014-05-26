8d12b16a2a40a1b381b254d9169abe8e08de5290
Brian Warsing <bcw@sfu.ca>
2014-05-26 10:02:55 -0700

    spec managedmac::systempreferences

---
+++ spec/classes/managedmac_systempreferences_spec.rb
@@ -0,0 +1,105 @@
+require 'spec_helper'
+
+describe "managedmac::systempreferences", :type => 'class' do
+
+  context "when passed no params" do
+    it { should compile.with_all_deps }
+  end
+
+  context "when passed BAD params" do
+    let(:params) do
+      { :allow_timemachine => 'This is not a Bool.' }
+    end
+
+    specify { expect { should compile }.to raise_error(Puppet::Error) }
+  end
+
+  context "when at least one parameter == true" do
+    let(:params) do
+      { :allow_energysaver => false,
+        :allow_datetime => false,
+        :allow_timemachine => true,
+      }
+    end
+
+    it do
+      should contain_macauthdb('system.preferences').with(
+        'group' => 'everyone',
+      )
+    end
+  end
+
+  context "when $allow_energysaver == true" do
+    let(:params) do
+      { :allow_energysaver => true,
+        :allow_datetime => false,
+        :allow_timemachine => false,
+      }
+    end
+
+    it do
+      should contain_macauthdb('system.preferences.energysaver').with(
+        'group' => 'everyone',)
+    end
+
+    it do
+      should contain_macauthdb('system.preferences.datetime').with(
+        'group' => 'admin',)
+    end
+
+    it do
+      should contain_macauthdb('system.preferences.timemachine').with(
+        'group' => 'admin',)
+    end
+  end
+
+  context "when $allow_datetime == true" do
+    let(:params) do
+      { :allow_energysaver => false,
+        :allow_datetime => true,
+        :allow_timemachine => false,
+      }
+    end
+
+    it do
+      should contain_macauthdb('system.preferences.energysaver').with(
+        'group' => 'admin',)
+    end
+
+    it do
+      should contain_macauthdb('system.preferences.datetime').with(
+        'group' => 'everyone',)
+    end
+
+    it do
+      should contain_macauthdb('system.preferences.timemachine').with(
+        'group' => 'admin',)
+    end
+  end
+
+  context "when $allow_timemachine == true" do
+    let(:params) do
+      { :allow_energysaver => false,
+        :allow_datetime => false,
+        :allow_timemachine => true,
+      }
+    end
+
+    it do
+      should contain_macauthdb('system.preferences.energysaver').with(
+        'group' => 'admin',)
+    end
+
+    it do
+      should contain_macauthdb('system.preferences.datetime').with(
+        'group' => 'admin',)
+    end
+
+    it do
+      should contain_macauthdb('system.preferences.timemachine').with(
+        'group' => 'everyone',)
+    end
+  end
+
+
+end
