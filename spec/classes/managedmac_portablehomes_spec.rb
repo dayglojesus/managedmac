d68797b3a169d1d905947a865520fe0e5c86c929
Brian Warsing <bcw@sfu.ca>
2014-05-27 13:05:06 -0700

    beef-up class spec a bit

--- spec/classes/managedmac_portablehomes_spec.rb
+++ spec/classes/managedmac_portablehomes_spec.rb
@@ -1,11 +1,86 @@
 require 'spec_helper'
 
 describe "managedmac::portablehomes", :type => 'class' do
-  
-  let(:params) do
-    { :enable => true }
+
+  context "when passed no params" do
+    it do
+      should contain_mobileconfig('managedmac.portablehomes.alacarte').with_ensure('absent')
+    end
   end
-  
-  it { should compile.with_all_deps }
+
+  context "when $menuextra has BAD param" do
+    let(:params) do
+      { :enable => true, :menuextra => 'on' }
+    end
+    specify do
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /Invalid parameter/)
+    end
+  end
+
+  context "when $syncPeriodSeconds has BAD param" do
+    let(:params) do
+      { :enable => true, :syncPeriodSeconds => 'foobar' }
+    end
+    specify do
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /not an Integer/)
+    end
+  end
+
+  context "when $syncPreferencesAtLogin has BAD param" do
+    let(:params) do
+      { :enable => true, :syncPreferencesAtLogin => 'foobar' }
+    end
+    specify do
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /Parameter Error: invalid value/)
+    end
+  end
+
+  context "when $loginPrefSyncConflictResolution has BAD param" do
+    let(:params) do
+      { :enable => true, :loginPrefSyncConflictResolution => 'foobar' }
+    end
+    specify do
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /Parameter Error: invalid value/)
+    end
+  end
+
+  context "when $excludedItems has BAD param" do
+    let(:params) do
+      { :enable => true, :excludedItems => 'foobar' }
+    end
+    specify do
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /Wrong arg type.*String instead of Hash/)
+    end
+  end
+
+  context "when $syncedFolders has BAD param" do
+    let(:params) do
+      { :enable => true, :syncedFolders => 'foobar' }
+    end
+    specify do
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /Wrong arg type.*String instead of Array/)
+    end
+  end
+
+  context "when passed good params" do
+    let(:params) do
+      { :enable => true }
+    end
+    it do
+      should contain_mobileconfig('managedmac.portablehomes.alacarte').with_ensure('present')
+    end
+  end
 
 end
