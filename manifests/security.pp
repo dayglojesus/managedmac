53dc0c370db271635ee2e5e4615f5d9f9ce15187
Brian Warsing <bcw@sfu.ca>
2014-06-03 09:43:44 -0700

    complete overhaul: - make ALL the param default values: undef - remove the enable param in favour of a calculated present/absent - make each of the payloads' keys dependent on a defined value

--- manifests/security.pp
+++ manifests/security.pp
@@ -2,27 +2,26 @@
 #
 # Leverages the Mobileconfig type to deploy a Security profile.
 #
-# === Parameters
+# All of the parameter defaults are 'undef' so including/containing the class
+# is not sufficient for confguration. You must activate the params by giving
+# them a value.
 #
-# [*enable*]
-#   --> Enable this class and install the Security profile.
-#   Type: Boolean
-#   Default: false
+# === Parameters
 #
 # [*ask_for_password*]
 #   --> Require a password after sleep or screensaver begins
 #   Type: Boolean
-#   Default: false
+#   Default: undef
 #
 # [*ask_for_password_delay*]
 #   --> The interval in seconds before the screensaver demands a password.
 #   Type: Integer
-#   Default: 0
+#   Default: undef
 #
 # [*disable_autologin*]
 #   --> Prevent users from using autologin during startup.
 #   Type: Boolean
-#   Default: false
+#   Default: undef
 #
 # [*gatekeeper_enable_assessment*]
 #   --> Only allow apps and packages from Mac App Store and identified
@@ -30,7 +29,7 @@
 #   packages to be installed. This equates to the 'Anywhere' UI setting. True
 #   means only allow trusted apps/packages.
 #   Type: Boolean
-#   Default: true
+#   Default: undef
 #
 # [*gatekeeper_allow_identified_developers*]
 #   --> Allow apps/packages from 'Mac App Store' or 'Mac App Store and
@@ -38,23 +37,23 @@
 #   'Mac App Store and identified developers'. False is Mac App Store ONLY.
 #   True is both.
 #   Type: Boolean
-#   Default: true
+#   Default: undef
 #
 # [*gatekeeper_disable_override*]
 #   --> Control disables the Finder's contextual menu that allows bypass of
 #   System Policy restrictions.
 #   Type: Boolean
-#   Default: false
+#   Default: undef
 #
 # [*dont_allow_lock_message_ui*]
 #   --> Don't allow user to set lock message
 #   Type: Boolean
-#   Default: false
+#   Default: undef
 #
 # [*dont_allow_password_reset_ui*]
 #   --> Don't allow user to change password
 #   Type: Boolean
-#   Default: false
+#   Default: undef
 #
 # === Variables
 #
@@ -67,8 +66,9 @@
 #
 #  # Example: defaults.yaml
 #  ---
-#  managedmac::security::enable: true
 #  managedmac::security::ask_for_password: true
+#  managedmac::security::ask_for_password_delay: 300
+#  managedmac::security::disable_autologin: true
 #
 # Then simply, create a manifest and include the class...
 #
@@ -79,8 +79,9 @@
 # something along these lines:
 #
 #  class { 'managedmac::security':
-#    enable            => true,
-#    ask_for_password  => true,
+#    ask_for_password        => true,
+#    ask_for_password_delay  => 300,
+#    disable_autologin       => true,
 #  }
 #
 # === Authors
@@ -93,61 +94,134 @@
 #
 class managedmac::security (
 
-  $enable                                 = false,
-  $ask_for_password                       = false,
-  $ask_for_password_delay                 = 0,
-  $disable_autologin                      = false,
-  $gatekeeper_enable_assessment           = true,
-  $gatekeeper_allow_identified_developers = true,
-  $gatekeeper_disable_override            = false,
-  $dont_allow_lock_message_ui             = false,
-  $dont_allow_password_reset_ui           = false,
+  $ask_for_password                       = undef,
+  $ask_for_password_delay                 = undef,
+  $disable_autologin                      = undef,
+  $gatekeeper_enable_assessment           = undef,
+  $gatekeeper_allow_identified_developers = undef,
+  $gatekeeper_disable_override            = undef,
+  $dont_allow_lock_message_ui             = undef,
+  $dont_allow_password_reset_ui           = undef,
 
 ) {
 
+  unless $ask_for_password == undef {
+    validate_bool ($ask_for_password)
+  }
+
+  unless $ask_for_password_delay == undef {
+    unless is_integer($ask_for_password_delay) {
+      fail("ask_for_password_delay not an Integer: ${ask_for_password_delay}")
+    }
+  }
+
+  unless $disable_autologin == undef {
+    validate_bool ($disable_autologin)
+  }
+
+  unless $gatekeeper_enable_assessment == undef {
+    validate_bool ($gatekeeper_enable_assessment)
+  }
+
+  unless $gatekeeper_allow_identified_developers == undef {
+    validate_bool ($gatekeeper_allow_identified_developers)
+  }
+
+  unless $gatekeeper_enable_assessment == undef {
+    validate_bool ($gatekeeper_enable_assessment)
+  }
+
+  unless $dont_allow_lock_message_ui == undef {
+    validate_bool ($dont_allow_lock_message_ui)
+  }
+
+  unless $dont_allow_password_reset_ui == undef {
+    validate_bool ($dont_allow_password_reset_ui)
+  }
+
+  $ask_for_password_delay_as_bool = $ask_for_password_delay ? {
+    undef   => false,
+    default => true
+  }
+
+  $enable = num2bool(
+    bool2num($ask_for_password) +
+    bool2num($ask_for_password_delay_as_bool) +
+    bool2num($disable_autologin) +
+    bool2num($gatekeeper_enable_assessment) +
+    bool2num($gatekeeper_allow_identified_developers) +
+    bool2num($gatekeeper_disable_override) +
+    bool2num($dont_allow_lock_message_ui) +
+    bool2num($dont_allow_password_reset_ui)
+  )
+
   validate_bool ($enable)
-  validate_bool ($ask_for_password)
-  validate_bool ($disable_autologin)
-  validate_bool ($gatekeeper_enable_assessment)
-  validate_bool ($gatekeeper_allow_identified_developers)
-  validate_bool ($gatekeeper_enable_assessment)
-  validate_bool ($dont_allow_lock_message_ui)
-  validate_bool ($dont_allow_password_reset_ui)
 
-  $systempolicy_managed_payload = {
-    'PayloadType'     => 'com.apple.systempolicy.managed',
-    'DisableOverride' => $gatekeeper_disable_override,
-  }
-
-  $systempolicy_control_payload = {
-    'PayloadType'               => 'com.apple.systempolicy.control',
-    'AllowIdentifiedDevelopers' => $gatekeeper_allow_identified_developers,
-    'EnableAssessment'          => $gatekeeper_enable_assessment,
-  }
-
-  $loginwindow_payload = {
-    'PayloadType'                                => 'com.apple.loginwindow',
-    'ChangePasswordDisabled'                     => $dont_allow_password_reset_ui,
-    'com.apple.login.mcx.DisableAutoLoginClient' => $disable_autologin,
-  }
+  # Puppet Selectors cannot deal with Hashes, so populate an empty one
+  # https://projects.puppetlabs.com/issues/14301
 
   $an_empty_hash = {}
 
   #######################################################################
+  # Handle System Policy Managed Payload
+  #######################################################################
+
+  $systempolicy_managed_01 = $gatekeeper_disable_override ? {
+    /true|false/ => hash('DisableOverride', $gatekeeper_disable_override),
+    default      => $an_empty_hash,
+  }
+  $systempolicy_managed_payload = merge(
+    {'PayloadType' => 'com.apple.systempolicy.managed'},
+    $systempolicy_managed_01
+  )
+
+  #######################################################################
+  # Handle System Policy Control Payload
+  #######################################################################
+
+  $systempolicy_control_01 = $gatekeeper_allow_identified_developers ? {
+    /true|false/ => hash('AllowIdentifiedDevelopers', $gatekeeper_allow_identified_developers),
+    default      => $an_empty_hash,
+  }
+  $systempolicy_control_02 = $gatekeeper_enable_assessment ? {
+    /true|false/ => hash('AllowIdentifiedDevelopers', $gatekeeper_enable_assessment),
+    default      => $an_empty_hash,
+  }
+  $systempolicy_control_payload = merge(
+    {'PayloadType' => 'com.apple.systempolicy.control'},
+    $systempolicy_control_01,
+    $systempolicy_control_02
+  )
+
+  #######################################################################
+  # Handle Loginwindow Payload
+  #######################################################################
+
+  $loginwindow_01 = $dont_allow_password_reset_ui ? {
+    /true|false/ => hash('ChangePasswordDisabled', $dont_allow_password_reset_ui),
+    default      => $an_empty_hash,
+  }
+  $loginwindow_02 = $disable_autologin ? {
+    /true|false/ => hash('com.apple.login.mcx.DisableAutoLoginClient', $disable_autologin),
+    default      => $an_empty_hash,
+  }
+  $loginwindow_payload = merge(
+    {'PayloadType' => 'com.apple.loginwindow'},
+    $loginwindow_01,
+    $loginwindow_02
+  )
+
+  #######################################################################
   # Handle Security Preferences Payload
-  # - if these keys exist in the Payload, the UI gets locked regardless
-  # of value. To fix this, we only add the keys to the Payload if the
-  # value of the options are true.
-  # - this is an Apple bug
   #######################################################################
 
   $security_preference_01 = $dont_allow_lock_message_ui ? {
-    true    => hash('dontAllowLockMessageUI', $dont_allow_lock_message_ui),
-    default => $an_empty_hash,
+    /true|false/ => hash('dontAllowLockMessageUI', $dont_allow_lock_message_ui),
+    default      => $an_empty_hash,
   }
   $security_preference_02 = $dont_allow_password_reset_ui ? {
-    true    => hash(['dontAllowPasswordResetUI', $dont_allow_password_reset_ui]),
-    default => $an_empty_hash,
+    /true|false/ => hash(['dontAllowPasswordResetUI', $dont_allow_password_reset_ui]),
+    default      => $an_empty_hash,
   }
   $security_preference_payload = merge(
     {'PayloadType' => 'com.apple.preference.security'},
@@ -157,16 +231,15 @@ class managedmac::security (
 
   #######################################################################
   # Handle Screensaver and Sleep Security Payload
-  # - fix the same behaviour as the com.apple.preference.security
   #######################################################################
 
   $screensaver_option_01 = $ask_for_password ? {
-    true    => hash(['askForPassword', $ask_for_password]),
-    default => $an_empty_hash
+    /true|false/ => hash(['askForPassword', $ask_for_password]),
+    default      => $an_empty_hash,
   }
   $screensaver_option_02 = $ask_for_password_delay ? {
-    true    => hash(['askForPasswordDelay', $ask_for_password_delay]),
-    default => $an_empty_hash
+    /\d+/   => hash(['askForPasswordDelay', $ask_for_password_delay]),
+    default => $an_empty_hash,
   }
   $screensaver_payload = merge(
     {'PayloadType' => 'com.apple.screensaver'},
