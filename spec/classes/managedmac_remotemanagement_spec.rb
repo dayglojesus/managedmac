9b0e174961a7b3c6d26f237a9529a1147963495c
Brian Warsing <bcw@sfu.ca>
2014-06-05 14:17:49 -0700

    add spec for managedmac::remotemanagement

---
+++ spec/classes/managedmac_remotemanagement_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe 'managedmac::remotemanagement', :type => 'class' do
+
+  context "when none of the params is set" do
+    it do
+      should contain_remotemanagement('apple_remote_desktop').with_ensure('stopped')
+    end
+  end
+
+  context "when passed a BAD param" do
+    let(:params) do
+      { :allow_all_users => 'a string', }
+    end
+    specify do
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /not a boolean/)
+    end
+  end
+
+  context "when $enable == false" do
+    let(:params) do
+      { :enable => false  }
+    end
+    it do
+      should contain_remotemanagement('apple_remote_desktop').with_ensure('stopped')
+    end
+  end
+
+  context "when $enable == true" do
+    let(:params) do
+      { :enable => true  }
+    end
+    it do
+      should contain_remotemanagement('apple_remote_desktop').with_ensure('running')
+    end
+  end
+
+end
