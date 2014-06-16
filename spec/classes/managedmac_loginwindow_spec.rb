53ee1a289b49ec440dc3b3aec9a831aea1eaf914
Brian Warsing <bcw@sfu.ca>
2014-06-16 16:07:14 -0700

    re-spec loginwindow class

--- spec/classes/managedmac_loginwindow_spec.rb
+++ spec/classes/managedmac_loginwindow_spec.rb
@@ -1,93 +1,63 @@
 require 'spec_helper'
 
 describe 'managedmac::loginwindow', :type => 'class' do
-  
-  # Test ensurability
-  context "when $ensure => 'absent'" do
-    let(:params) do
-      { :ensure => 'absent' }
-    end
-    
+
+  context "when NO params are passed" do
     it { should contain_mobileconfig('managedmac.loginwindow.alacarte')\
       .with_ensure('absent') }
-  end  
-  
-  context "when $ensure is invalid" do
-    let(:params) do
-      { :ensure => 'whatever' }
-    end
-    
-    specify do 
-      expect { 
-        should compile 
-      }.to raise_error(Puppet::Error, /Parameter Error/)
-    end
   end
 
-  context "when the banner_text param is not a String" do
-    let(:params) do
-      { :banner_text => ['This is an Array'] }
+  context "when passed a BAD param" do
+
+    context "when $allow_list is NOT an array" do
+      let(:params) do
+        { :allow_list => 'foobar' }
+      end
+      specify { expect { should compile }.to raise_error(Puppet::Error) }
     end
-    specify { expect { should compile }.to raise_error(Puppet::Error) }
-  end
-  
-  context "when the show_full_name param is not a Boolean" do
-    let(:params) do
-      { :show_full_name => ['This is an Array'] }
+
+    context "when $deny_list is NOT an array" do
+      let(:params) do
+        { :deny_list => 'foobar' }
+      end
+      specify { expect { should compile }.to raise_error(Puppet::Error) }
     end
-    specify { expect { should compile }.to raise_error(Puppet::Error) }
-  end
-  
-  context "when the show_buttons param is not a Boolean" do
-    let(:params) do
-      { :show_buttons => ['This is an Array'] }
+
+    context "when $disable_console_access is NOT bool" do
+      let(:params) do
+        { :disable_console_access => 'foobar' }
+      end
+      specify { expect { should compile }.to raise_error(Puppet::Error) }
     end
-    specify { expect { should compile }.to raise_error(Puppet::Error) }
-  end
-  
-  context "when the options param is not a Hash" do
-    message = 'This is a loginwindow.'
-    let(:params) do
-      { :options => message }
+
+    context "when $loginwindow_text is NOT a String" do
+      let(:params) do
+        { :loginwindow_text => [] }
+      end
+      specify { expect { should compile }.to raise_error(Puppet::Error) }
     end
-    
-    specify { expect { should compile }.to raise_error(Puppet::Error) }
-  end
-  
-  context "when all the params are empty" do
-    specify { expect { should compile }.to raise_error(Puppet::Error) }
-  end
-  
-  context "when there is at least one param specified" do
-    let(:params) do
-      { :show_buttons => true }
+
+    context "when $retries_until_hint is NOT an Integer" do
+      let(:params) do
+        { :retries_until_hint => 'foobar' }
+      end
+      specify { expect { should compile }.to raise_error(Puppet::Error) }
     end
-    
-    it { should contain_mobileconfig('managedmac.loginwindow.alacarte')\
-      .with_content(/RestartDisabled.*true/) }
+
   end
-  
-  context "when the options param is a Hash with at least one key" do
-    message = 'This is a loginwindow.'
+
+  context "when passed one or more GOOD parameters" do
     let(:params) do
-      { :options => { 'BannerText' => message } }
-    end
-    
-    it { should contain_mobileconfig('managedmac.loginwindow.alacarte')\
-      .with_content(/#{message}/) }
-  end
-  
-  context "when params and options Hash contain the same keys" do
-    options_message = 'OPTIONS: I am a loginwindow.'
-    params_message  = 'PARAMS:  I am a loginwindow.'
-    let(:params) do
-      { 
-        :banner_text => params_message,
-        :options     => { 'BannerText' => options_message } 
+      {
+        :loginwindow_text => 'An important message.',
+        :disable_console_access => true,
+        :show_name_and_password_fields => true,
       }
     end
     it { should contain_mobileconfig('managedmac.loginwindow.alacarte')\
-      .with_content(/#{params_message}/) }
+      .with_ensure('present') }
+    it { should contain_mobileconfig('managedmac.loginwindow.alacarte')\
+      .with_content(/An important message/) }
   end
-  
+
 end
