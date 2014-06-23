f6fea151e3dcf70fc11bcf7355ed5891551eccb3
Brian Warsing <bcw@sfu.ca>
2014-06-23 10:36:56 -0700

    major refactor: - remove defer param (was useless. defer must always be true) - make param defaults all undef - re-org param checking - remove useless PayloadType com.apple.system.managed - refactor mobileconfig content to use function as per other classes

--- manifests/filevault.pp
+++ manifests/filevault.pp
@@ -1,71 +1,53 @@
 # == Class: managedmac::filevault
 #
-# Leverages the Mobileconfig type to deploy a Filevault profile. It provides
+# Leverages the Mobileconfig type to deploy a FileVault 2 profile. It provides
 # only a subset of the options available to the profile and does not conform to
 # the Apple defaults. Read the documentation.
 #
 # === Parameters
 #
-# This class takes 6 parameters:
-#
 # [*enable*]
-#   --> Set to 'On' to enable FileVault. Set to 'Off' to disable FileVault.
-#   This value is required.
+#   --> Whether to enable FileVault or not.
 #   Type: Boolean
-#   Default: false
-#
-# [*defer*]
-#   --> Set to true to defer enabling FileVault until the designated user logs
-#   out.
-#   Type: Boolean
-#   Default: true
 #
 # [*use_recovery_key*]
-#   --> Set to true to create a personal recovery key.
+#   Set to true to create a personal recovery key.
 #   Type: Boolean
-#   Default: true
 #
 # [*show_recovery_key*]
-#   --> Set to true to display the personal recovery key to the user after
+#   Set to true to display the personal recovery key to the user after
 #   FileVault is enabled.
 #   Type: Boolean
-#   Default: false
 #
 # [*output_path*]
-#   --> Path to the location where the recovery key and computer information
+#   Path to the location where the recovery key and computer information
 #   plist will be stored.
 #   Type: String
-#   Default: /private/var/root/fdesetup_output.plist
 #
 # [*use_keychain*]
-#   --> If set to true and no certificate information is provided in this
+#   If set to true and no certificate information is provided in this
 #   payload, the keychain already created at
 #   /Library/Keychains/FileVaultMaster.keychain will be used when the
 #   institutional recovery key is added.
 #   Type: Boolean
-#   Default: true
 #
 # [*keychain_file*]
-#   --> An absolute path or puppet:/// style URI from whence to gather an FVMI.
+#   An absolute path or puppet:/// style URI from whence to gather an FVMI.
 #   It will install and manage /Library/Keychains/FileVaultMaster.keychain.
 #   Only works when $use_keychain is true.
 #   Type: String
-#   Default: empty
 #
 # [*destroy_fv_key_on_standby*]
-#   --> Prevent saving the key across standby modes.
+#   Prevent saving the key across standby modes.
 #   Type: Boolean
-#   Default: false
 #
 # [*dont_allow_fde_disable*]
-#   --> Prevent users from disabling FDE.
+#   Prevent users from disabling FDE.
 #   Type: Boolean
-#   Default: false
 #
 # [*remove_fde*]
-#   --> Removes FDE if $enable is false and the disk is encrypted.
+#   Removes FDE if $enable is false and the disk is encrypted.
 #   Type: Boolean
-#   Default: false
 #
 # === Variables
 #
@@ -79,7 +61,6 @@
 #  # Example: defaults.yaml
 #  ---
 # managedmac::filevault::enable: true
-# managedmac::filevault::defer: true
 # managedmac::filevault::use_recovery_key: true
 # managedmac::filevault::show_recovery_key: true
 #
@@ -93,7 +74,6 @@
 #
 #  class { 'managedmac::filevault':
 #    enable => true,
-#    defer  => true,
 #  }
 #
 # === Authors
@@ -104,82 +84,116 @@
 #
 # Copyright 2014 Simon Fraser University, unless otherwise noted.
 #
+
+# $enable                     = false,
+# $use_recovery_key           = true,
+# $show_recovery_key          = false,
+# $output_path                = '/private/var/root/fdesetup_output.plist',
+# $use_keychain               = false,
+# $keychain_file              = '',
+# $destroy_fv_key_on_standby  = false,
+# $dont_allow_fde_disable     = false,
+# $remove_fde                 = false,
+
 class managedmac::filevault (
 
-  $enable                     = false,
-  $defer                      = true,
-  $use_recovery_key           = true,
-  $show_recovery_key          = false,
-  $output_path                = '/private/var/root/fdesetup_output.plist',
-  $use_keychain               = false,
-  $keychain_file              = '',
-  $destroy_fv_key_on_standby  = false,
-  $dont_allow_fde_disable     = false,
-  $remove_fde                 = false,
+  $enable                     = undef,
+  $use_recovery_key           = undef,
+  $show_recovery_key          = undef,
+  $output_path                = undef,
+  $use_keychain               = undef,
+  $keychain_file              = undef,
+  $destroy_fv_key_on_standby  = undef,
+  $dont_allow_fde_disable     = undef,
+  $remove_fde                 = undef,
 
 ){
 
-  validate_bool ($enable)
-  validate_bool ($defer)
-  validate_bool ($use_recovery_key)
-  validate_bool ($show_recovery_key)
-  validate_bool ($use_keychain)
-  validate_bool ($destroy_fv_key_on_standby)
-  validate_bool ($dont_allow_fde_disable)
-  validate_absolute_path ($output_path)
+  unless $enable == undef {
 
-  if $use_keychain == true {
+    validate_bool ($enable)
 
-    if $keychain_file =~ /^puppet:/ {
-      validate_re ($keychain_file,
-        'puppet:(\/{3}(\w+\/)+\w+|\/{2}(\w+\.)+(\w+\/)+\w+)')
-    } else {
-      validate_absolute_path ($keychain_file)
+    unless $use_recovery_key == undef {
+      validate_bool ($use_recovery_key)
     }
 
-    file { 'filevault_master_keychain':
-      ensure => file,
-      owner  => root,
-      group  => wheel,
-      mode   => 0644,
-      path   => '/Library/Keychains/FileVaultMaster.keychain',
-      source => "${keychain_file}";
+    unless $show_recovery_key == undef {
+      validate_bool ($show_recovery_key)
     }
 
-  }
-
-  $systempolicy_payload = { 'PayloadType' => 'com.apple.systempolicy.managed' }
-
-  $filevault_payload = { 'PayloadType' => 'com.apple.MCX.FileVault2',
-    'Enable'          => 'On',
-    'Defer'           => $defer,
-    'UseRecoveryKey'  => $use_recovery_key,
-    'ShowRecoveryKey' => $show_recovery_key,
-    'OutputPath'      => $output_path,
-    'UseKeychain'     => $use_keychain,
-  }
-
-  $mcx_payload = { 'PayloadType' => 'com.apple.MCX',
-    'DestroyFVKeyOnStandby' => $destroy_fv_key_on_standby,
-    'dontAllowFDEDisable'   => $dont_allow_fde_disable,
-  }
-
-  mobileconfig { 'managedmac.filevault.alacarte':
-    ensure => $enable ? {
-      true     => 'present',
-      default  => 'absent',
-    },
-    content      => [$systempolicy_payload, $mcx_payload, $filevault_payload],
-    displayname  => 'Managed Mac: FileVault 2',
-    description  => 'FileVault 2 configuration. Installed by Puppet.',
-    organization => 'Simon Fraser University',
-  }
-
-  if ($enable == false) and ($::filevault_active == true) and ($remove_fde == true)  {
-    exec { 'decrypt_the_disk':
-      command => '/usr/bin/fdesetup disable',
-      returns => [0,1],
+    unless $use_keychain == undef {
+      validate_bool ($use_keychain)
     }
+
+    unless $destroy_fv_key_on_standby == undef {
+      validate_bool ($destroy_fv_key_on_standby)
+    }
+
+    unless $dont_allow_fde_disable == undef {
+      validate_bool ($dont_allow_fde_disable)
+    }
+
+    unless $remove_fde == undef {
+      validate_bool ($remove_fde)
+    }
+
+    unless $output_path == undef {
+      validate_absolute_path ($output_path)
+    }
+
+    if $use_keychain == true {
+      if $keychain_file =~ /^puppet:/ {
+        validate_re ($keychain_file,
+          'puppet:(\/{3}(\w+\/)+\w+|\/{2}(\w+\.)+(\w+\/)+\w+)')
+      } else {
+        validate_absolute_path ($keychain_file)
+      }
+
+      file { 'filevault_master_keychain':
+        ensure => file,
+        owner  => root,
+        group  => wheel,
+        mode   => 0644,
+        path   => '/Library/Keychains/FileVaultMaster.keychain',
+        source => "${keychain_file}";
+      }
+    }
+
+    $params = {
+      'com.apple.MCX.FileVault2' => {
+        'Enable'          => 'On',
+        'Defer'           => true,
+        'UseRecoveryKey'  => $use_recovery_key,
+        'ShowRecoveryKey' => $show_recovery_key,
+        'OutputPath'      => $output_path,
+        'UseKeychain'     => $use_keychain,
+      },
+      'com.apple.MCX' => {
+        'DestroyFVKeyOnStandby' => $destroy_fv_key_on_standby,
+        'dontAllowFDEDisable'   => $dont_allow_fde_disable,
+      },
+    }
+
+    $content = process_filevault_params($params)
+
+    mobileconfig { 'managedmac.filevault.alacarte':
+      ensure => $enable ? {
+        true     => 'present',
+        default  => 'absent',
+      },
+      content      => $content,
+      displayname  => 'Managed Mac: FileVault 2',
+      description  => 'FileVault 2 configuration. Installed by Puppet.',
+      organization => 'Simon Fraser University',
+    }
+
+    if ($enable == false) and ($::filevault_active == true) and ($remove_fde == true)  {
+      exec { 'decrypt_the_disk':
+        command => '/usr/bin/fdesetup disable',
+        returns => [0,1],
+      }
+    }
+
   }
 
 }
