9f9d505fef0d51d4eeca72030a87d87972539cc3
Brian Warsing <bcw@sfu.ca>
2014-06-23 15:28:23 -0700

    rename functions associated with the portablehomes class

--- lib/puppet/parser/functions/portablehomes_excluded_items.rb
+++ lib/puppet/parser/functions/portablehomes_excluded_items.rb
@@ -1,16 +1,16 @@
 module Puppet::Parser::Functions
-  newfunction(:excluded_items, :type => :rvalue, :doc => <<-EOS
+  newfunction(:portablehomes_excluded_items, :type => :rvalue, :doc => <<-EOS
 Returns a Array of properly formatted excludeItems Hashes.
     EOS
   ) do |args|
 
     if args.size != 1
-      e = "excluded_items(): Too many args! (#{args.size} instead of 1)"
+      e = "portablehomes_excluded_items(): Too many args! (#{args.size} instead of 1)"
       raise(Puppet::ParseError, e)
     end
 
     unless args[0].is_a? Hash
-      e = "excluded_items(): Wrong arg type! (#{args[0].class} instead of Hash)"
+      e = "portablehomes_excluded_items(): Wrong arg type! (#{args[0].class} instead of Hash)"
       raise(Puppet::ParseError, e)
     end
 
