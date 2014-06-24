d2a352c190a8fc33005187ce5081a46d63f05762
Brian Warsing <bcw@sfu.ca>
2014-06-24 12:03:55 -0700

    make all params optional for users class

--- manifests/users.pp
+++ manifests/users.pp
@@ -8,8 +8,6 @@
 #
 # === Parameters
 #
-# There 2 parameters, the $accounts parameter is required.
-#
 # [*accounts*]
 #   This is a Hash of Hashes.
 #   The hash should be in the form { title => { parameters } }.
@@ -76,24 +74,33 @@
 #
 # Copyright 2014 Simon Fraser University, unless otherwise noted.
 #
-class managedmac::users ($accounts, $defaults = {}) {
+class managedmac::users (
 
-  validate_hash ($accounts)
-  validate_hash ($defaults)
+  $accounts = undef,
+  $defaults = {},
 
-  if empty ($accounts) {
-    fail('Parameter Error: $accounts is empty')
-  } else {
-    # Cheating: validate that the value for each key is itself a Hash
-    $check_hash = inline_template("<%= @accounts.reject! {
-      |x| x.respond_to? :key } %>")
+) {
 
-    unless empty($check_hash) {
-      fail("Account Error: Failed to parse one or more account data objects:
-        ${check_hash}")
+  unless $accounts == undef {
+
+    validate_hash ($accounts)
+    validate_hash ($defaults)
+
+    if empty ($accounts) {
+      fail('Parameter Error: $accounts is empty')
+    } else {
+      # Cheating: validate that the value for each key is itself a Hash
+      $check_hash = inline_template("<%= @accounts.reject! {
+        |x| x.respond_to? :key } %>")
+
+      unless empty($check_hash) {
+        fail("Account Error: Failed to parse one or more account data objects:
+          ${check_hash}")
+      }
+
+      create_resources(user, $accounts, $defaults)
     }
 
-    create_resources(user, $accounts, $defaults)
   }
 
 }
