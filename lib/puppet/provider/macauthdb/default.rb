a5ac8818f856ca66b04139382db362661366f67c
Brian Warsing <dayglojesus@gmail.com>
2014-09-15 20:17:06 -0700

    basic overhaul complete - provider overhauled to properly implement rules - previous implementation was incomplete - outstanding issues: defaults for rules are too aggressive and needs some general refactoring

--- lib/puppet/provider/macauthdb/default.rb
+++ lib/puppet/provider/macauthdb/default.rb
@@ -17,20 +17,26 @@ Puppet::Type.type(:macauthdb).provide(:default) do
     'flags', 'tries', 'version', 'created', 'modified', 'hash',
     'identifier', 'requirement', 'comment']
 
-  PROPERTIES = [ :allow_root, :authenticate_user,
-    :auth_class, :auth_type, :comment, :group, :kofn, :mechanisms, :rule,
-    :session_owner, :shared, :timeout, :tries ]
+  ATTRIBS = %w(id name type class "group" kofn timeout flags tries comment)
+
+  PROPERTIES = [ :auth_class, :auth_type, :group, :mechanisms, :rule,
+    :comment, :kofn, :timeout, :tries, :shared, :allow_root,
+    :session_owner, :authenticate_user, :extract_password, :entitled,
+    :entitled_group, :require_apple_signed, :vpn_entitled_group ]
 
   FLAGS = [ 'shared', 'allow-root', 'session-owner', 'authenticate-user',
     'extract-password', 'entitled', 'entitled-group', 'require-apple-signed',
     'vpn-entitled-group' ]
 
+  CLASSES = ['user', 'rule', 'evaluate-mechanisms', 'allow', 'deny']
+
+  TYPES = ['right', 'rule']
+
   class << self
 
     def instances
       list_all_rights_and_rules.collect do |rule|
-        rule_properties = get_resource_properties(rule)
-        new(rule_properties)
+        new(rule)
       end
     end
 
@@ -44,25 +50,82 @@ Puppet::Type.type(:macauthdb).provide(:default) do
     end
 
     def list_all_rights_and_rules
-      exec_sql_statement 'select * from rules'
+      begin
+        @dbconn = SQLite3::Database.new(AUTH_DB)
+        all_rules = exec_sql_statement @dbconn, "SELECT #{ATTRIBS.join(', ')} FROM rules"
+        processed = all_rules.collect do |rule|
+          rule_properties = get_resource_properties(@dbconn, rule)
+        end
+      rescue Exception => e
+        raise Puppet::Error, "Could not exec SQL: `#{e}`"
+      ensure
+        @dbconn.close if @dbconn
+      end
+      @dbconn.close if @dbconn
+      processed
+    end
+
+    def exec_sql_statement(dbconn, string, *args)
+      begin
+        op      = dbconn.prepare string
+        results = op.execute(*args)
+        array   = results.to_a
+        op.close if op
+      rescue Exception => e
+        raise Puppet::Error, "Could not exec SQL: `#{e}`"
+      end
+      array
+    end
+
+    # SQL taken from the Apple source code
+    # http://opensource.apple.com/source/Security/Security-55471/authd/rule.c
+    def get_delegates(dbconn, rule_id)
+      expr = "SELECT rules.* \
+              FROM rules JOIN delegates_map \
+              ON rules.id = delegates_map.d_id \
+              WHERE delegates_map.r_id = ? \
+              ORDER BY delegates_map.ord ASC"
+
+      exec_sql_statement(dbconn, expr, rule_id).map { |r| r[1] }
+    end
+
+    # SQL taken from the Apple source code
+    # http://opensource.apple.com/source/Security/Security-55471/authd/rule.c
+    def get_mechanisms(dbconn, rule_id)
+      expr = "SELECT mechanisms.* \
+              FROM mechanisms \
+              JOIN mechanisms_map ON mechanisms.id = mechanisms_map.m_id \
+              WHERE mechanisms_map.r_id = ? \
+              ORDER BY mechanisms_map.ord ASC"
+
+      exec_sql_statement(dbconn, expr, rule_id).map { |r| r[1..2].join(':') }
     end
 
     # Return the Puppet resource as a Hash
-    def get_resource_properties(array)
+    def get_resource_properties(dbconn, array)
       return {} if array.nil?
-      row = process_db_row array
+      row = process_db_row dbconn, array
       row[:ensure] = :present
       row
     end
 
-    def process_db_row(array)
-      Hash[SCHEMA.zip(array)].inject({}) do |memo, (k,v)|
-        unless v.nil? or k =~ /created|modified|hash|identifier|requirement|id|version/
+    def process_db_row(dbconn, array)
+      Hash[ATTRIBS.zip(array)].inject({}) do |memo, (k,v)|
+        if k.eql?('id')
+          mechanisms        = get_mechanisms(dbconn, v)
+          memo[:mechanisms] = mechanisms unless mechanisms.empty?
+          rule              = get_delegates(dbconn, v)
+          memo[:rule]       = rule unless rule.empty?
+        end
+
+        unless v.nil? or k =~ /id/
           case k
+          when '"group"'
+            memo[:group] = v
           when 'class'
-            memo["auth_#{k}".to_sym] = convert_class v
+            memo["auth_#{k}".to_sym] = convert_int_to_class v
           when 'type'
-            memo["auth_#{k}".to_sym] = convert_type v
+            memo["auth_#{k}".to_sym] = convert_int_to_type v
           when 'flags'
             c = memo[:auth_class]
             unpack_flags(c, v).each { |k,v| memo[k.gsub('-', '_').to_sym] = v }
@@ -74,14 +137,24 @@ Puppet::Type.type(:macauthdb).provide(:default) do
       end
     end
 
-    def convert_class(int)
-      classes = ['user', 'rule', 'evaluate-mechanism', 'allow', 'deny']
-      classes[int - 1]
+    def convert_int_to_class(int)
+      return 'user' unless int
+      CLASSES[int - 1]
     end
 
-    def convert_type(int)
-      types = ['right', 'rule']
-      types[int - 1]
+    def convert_int_to_type(int)
+      return 'right' unless int
+      TYPES[int - 1]
+    end
+
+    def convert_class_to_int(string)
+      return 1 unless string
+      CLASSES.index(string) + 1
+    end
+
+    def convert_type_to_int(string)
+      return 1 unless string
+      TYPES.index(string) + 1
     end
 
     # Unpack the 'flags' Integer
@@ -98,7 +171,7 @@ Puppet::Type.type(:macauthdb).provide(:default) do
         case auth_class
         when 'user'
           memo[key] = value if value or (0..3).member?(i)
-        when 'evaluate-mechanism'
+        when 'evaluate-mechanisms'
           memo[key] = value if value or i == 0
         else
           memo[key] = value if value
@@ -111,22 +184,8 @@ Puppet::Type.type(:macauthdb).provide(:default) do
     # - accepts a Hash of normalized data
     def pack_flags(flags)
       FLAGS.each_with_index.inject(0) do |memo,(key,i)|
-        memo |= i if flags[key]
+        memo |= (1 << i) if flags[key]
         memo
-      end
-    end
-
-    def exec_sql_statement(string, *args)
-      begin
-        db = SQLite3::Database.new(AUTH_DB)
-        op = db.prepare string
-        results = op.execute(*args)
-        results.to_a
-      rescue SQLite3::Exception => e
-        raise Puppet::Error, "Could not exec SQL: `#{e}`"
-      ensure
-        op.close if op
-        db.close if db
       end
     end
 
@@ -159,11 +218,13 @@ Puppet::Type.type(:macauthdb).provide(:default) do
 
   def normalize_resource_data
     PROPERTIES.inject({}) do |memo,key|
-      value = resource[key].is_a?(Symbol) ? resource[key].to_s : resource[key]
+      source = @property_hash.empty? ? @resource : @property_hash
+      value  = source[key].is_a?(Symbol) ? source[key].to_s : source[key]
       if value
         key = key.to_s.gsub('_', '-')
         key.gsub!('auth-class', 'class')
-        memo[key] = value unless key.eql? 'auth-type'
+        key.gsub!('auth-type', 'type')
+        memo[key] = value
       end
       memo
     end
@@ -171,14 +232,14 @@ Puppet::Type.type(:macauthdb).provide(:default) do
 
   # Use `security` to create or modify the right or rule
   def security_create_right(data)
-    plist = CFPropertyList::List.new
-    plist.value = CFPropertyList.guess(data)
-    tmp = Tempfile.new('puppet_macauthdb')
+    plist        = CFPropertyList::List.new
+    plist.value  = CFPropertyList.guess(data)
+    tmp          = Tempfile.new('puppet_macauthdb')
     begin
       plist.save(tmp.path, CFPropertyList::List::FORMAT_XML)
       tmp.close
       cmds = [:security, "authorizationdb", "write", resource[:name]]
-      execute(cmds, :failonfail => false, :combine => false, 
+      execute(cmds, :failonfail => false, :combine => false,
         :stdinfile => tmp.path.to_s)
     rescue Errno::EACCES => e
       raise Puppet::Error.new("Cannot save right to #{tmp.path}: #{e}")
@@ -188,36 +249,92 @@ Puppet::Type.type(:macauthdb).provide(:default) do
   end
 
   def sql_create_rule(data)
-    sql = {
-      'name'     => resource[:name],
+    name = resource[:name]
+    formatted_rule = {
+      'name'     => name,
       'type'     => 2,
-      'class'    => data['class'] || 1,
+      'class'    => self.class.convert_class_to_int(data['class']) || 1,
       'group'    => data['group'],
-      'timeout'  => 2147483647,
+      'timeout'  => data['timeout'] || 0,
       'flags'    => self.class.pack_flags(data),
-      'tries'    => 10000,
-      'version'  => data['version'],
+      'tries'    => data['tries'],
+      'version'  => data['version'] || 0,
       'created'  => 407288069.183469,
       'modified' => 407288069.183469,
       'comment'  => data['comment'],
     }
-    columns      = sql.keys.collect { |x| "`#{x}`" }.join(',')
-    values       = sql.values
-    placeholders = (['?'] * sql.values.size).join(',')
-    statement = %Q{replace into rules (#{columns}) values (#{placeholders})}
 
-    self.class.exec_sql_statement statement, values
+    begin
+      @dbconn    = SQLite3::Database.new(AUTH_DB)
+      name_query = %Q{SELECT * FROM rules WHERE name = '#{name}'}
+      result     = self.class.exec_sql_statement(@dbconn, name_query).first
+      the_rule   = result.nil? ? {} : Hash[SCHEMA.zip(result)] || {}
+
+      unless the_rule.merge(formatted_rule) == the_rule
+        statement = if the_rule.empty?
+          columns      = formatted_rule.keys.collect { |x| "`#{x}`" }.join(',')
+          placeholders = (['?'] * formatted_rule.values.size).join(',')
+          %Q{INSERT INTO rules (#{columns}) VALUES (#{placeholders})}
+        else
+          formatted_rule.delete('name')
+          columns = formatted_rule.keys.map { |k| "`#{k}` = ?" }
+          %Q{UPDATE rules SET #{columns.join(', ')}}
+        end
+
+        self.class.exec_sql_statement(@dbconn, statement, formatted_rule.values)
+        result     = self.class.exec_sql_statement(@dbconn, name_query).first
+        the_rule   = result.nil? ? {} : Hash[SCHEMA.zip(result)] || {}
+      end
+
+      if data['mechanisms']
+        delete = "DELETE FROM mechanisms_map WHERE r_id = ?"
+        self.class.exec_sql_statement(@dbconn, delete, the_rule['id'])
+
+        unless data['mechanisms'].empty?
+          mechs = data['mechanisms'].map do |value|
+            plugin, param = value.split(':')
+            query = %Q{SELECT * FROM mechanisms WHERE plugin = ? and param = ?}
+            self.class.exec_sql_statement(@dbconn, query, [plugin, param]).last
+          end
+
+          mapping = "INSERT INTO mechanisms_map VALUES (?,?,?)"
+          mechs.each_with_index do |mech, i|
+            row = [the_rule['id'], mech[0], i]
+            self.class.exec_sql_statement(@dbconn, mapping, row)
+          end
+        end
+      end
+
+      if data['rule']
+        delete = "DELETE FROM delegates_map WHERE r_id = ?"
+        self.class.exec_sql_statement(@dbconn, delete, the_rule['id'])
+
+        unless data['rule'].empty?
+          rules = data['rule'].map do |name|
+            query = %Q{SELECT * FROM rules WHERE name = ?}
+            self.class.exec_sql_statement(@dbconn, query, name).first
+          end
+          mapping = "INSERT INTO delegates_map VALUES (?,?,?)"
+          rules.each_with_index do |rule, i|
+            row = [the_rule['id'], rule[0], i]
+            self.class.exec_sql_statement(@dbconn, mapping, row)
+          end
+        end
+      end
+    rescue Exception => e
+      raise Puppet::Error, "Could not exec SQL: `#{e}`"
+    end
+
   end
 
-  def retreive_defaults
+  def retreive_defaults(type)
     plist = CFPropertyList::List.new(:file => DEFAULTS)
     data  = CFPropertyList.native_types(plist.value)
-    type  = (resource[:auth_type] || 'right').to_s + 's'
-    data[type][resource[:name]]
+    data[type + 's'][resource[:name]] || { :name => resource[:name] }
   end
 
   def create_by_type_with_data(type, data)
-    if type == :rule
+    if type.to_s.eql?('rule')
       sql_create_rule(data)
     else
       security_create_right(data)
@@ -225,22 +342,29 @@ Puppet::Type.type(:macauthdb).provide(:default) do
   end
 
   def flush
+    type = @property_hash[:auth_type] || resource[:auth_type]
     case @property_flush[:ensure] || :present
     when :absent
       security_remove_right_or_rule
     when :present
-      data = normalize_resource_data
-      create_by_type_with_data resource[:auth_type], data
+      create_by_type_with_data type, normalize_resource_data
     when :default
-      data = retreive_defaults
-      create_by_type_with_data resource[:auth_type], data
+      create_by_type_with_data type, retreive_defaults(type)
     else
       raise Puppet::Error, "Invalid action passed to ensure param: `#{action}`"
     end
 
-    query = %Q{select * from rules where name is "#{resource[:name]}"}
-    results = self.class.exec_sql_statement query
-    @property_hash = self.class.get_resource_properties(results.first)
+    begin
+      query = %Q{SELECT * FROM rules WHERE name IS "#{resource[:name]}"}
+      @dbconn = SQLite3::Database.new(AUTH_DB)
+      results = self.class.exec_sql_statement @dbconn, query
+      @property_hash = self.class.get_resource_properties(@dbconn, results.first)
+    rescue Exception => e
+      raise Puppet::Error, "Could not exec SQL: `#{e}`"
+    ensure
+      @dbconn.close if @dbconn
+    end
+
   end
 
 end
