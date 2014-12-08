9beba6476a68d68dec8583f14fb09413bc9e7846
Brian Warsing <dayglojesus@gmail.com>
2014-12-07 17:56:11 -0800

    add new fact: macaddress_primary - returns MAC of first builtin, non-expansion bus interface - fix issue #52

---
+++ lib/facter/macaddress_primary.rb
@@ -0,0 +1,17 @@
+require 'puppet'
+require 'cfpropertylist'
+
+Facter.add("macaddress_primary") do
+  confine :operatingsystem => :darwin
+  setcode do
+    file    = "/Library/Preferences/SystemConfiguration/NetworkInterfaces.plist"
+    plist   = CFPropertyList::List.new(:file => file)
+    native  = CFPropertyList.native_types(plist.value)
+    devices = native['Interfaces'].select { |d|
+         d['IOBuiltin'] and
+           d['IOInterfaceNamePrefix'].eql?('en') and not
+             d['IOPathMatch'] =~ /thunderbolt|usb|firewire/i
+    }.sort_by { |d| d['IOInterfaceUnit'] }
+    devices[0]['IOMACAddress'].unpack('H*').first.scan(/../).join(':')
+  end
+end
