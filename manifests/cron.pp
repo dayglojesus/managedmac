268c109ea424a9d01c3b215fc2b3cf574a3f9616
Brian Warsing <dayglojesus@gmail.com>
2014-12-07 19:55:46 -0800

    add new raw constructor: managedmac::cron - fixes issue #47

---
+++ manifests/cron.pp
@@ -0,0 +1,92 @@
+# == Class: managedmac::cron
+#
+# Dynamically create Puppet Cron resources using the Puppet built-in
+# 'create_resources' function.
+#
+# We do some validation of data, but the usual caveats apply: garbage in,
+# garbage out.
+#
+# === Parameters
+#
+# [*jobs*]
+#   This is a Hash of Hashes.
+#   The hash should be in the form { title => { parameters } }.
+#   See http://tinyurl.com/7783b9l, and the examples below for details.
+#   Type: Hash
+#
+# [*defaults*]
+#   A Hash that defines the default values for the resources created.
+#   See http://tinyurl.com/7783b9l, and the examples below for details.
+#   Type: Hash
+#
+# === Variables
+#
+# Not applicable
+#
+# === Examples
+#
+# This class was designed to be used with Hiera. As such, the best way to pass
+# options is to specify them in your Hiera datadir:
+#
+# # Example: defaults.yaml
+# ---
+# managedmac::cron::jobs:
+#  logrotate:
+#    command: '/usr/sbin/logrotate'
+#    user:    'root'
+#    hour:    2
+#    minute:  0
+#
+# Then simply, create a manifest and include the class...
+#
+#  # Example: my_manifest.pp
+#  include managedmac::cron
+#
+# If you just wish to test the functionality of this class, you could also do
+# something along these lines:
+#
+#  # Create some Hashes
+#  $defaults = { user => 'root', hour => 2, minute => 0 }
+#  $jobs = {
+#     'logrotate' => { 'command' => '/usr/bin/who > /tmp/who.dump' },
+#  }
+#
+#  class { 'managedmac::cron':
+#    jobs     => $jobs,
+#    defaults => $defaults,
+#  }
+#
+# === Authors
+#
+# Brian Warsing <bcw@sfu.ca>
+#
+# === Copyright
+#
+# Copyright 2014 Simon Fraser University, unless otherwise noted.
+#
+class managedmac::cron (
+
+  $jobs     = {},
+  $defaults = {},
+
+) {
+
+  unless empty ($jobs) {
+
+    validate_hash ($jobs)
+    validate_hash ($defaults)
+
+    # Cheating: validate that the value for each key is itself a Hash
+    $check_hash = inline_template("<%= @jobss.reject! {
+      |x| x.respond_to? :key } %>")
+
+    unless empty($check_hash) {
+      fail("Account Error: Failed to parse one or more account data \
+commands: ${check_hash}")
+    }
+
+    create_resources(cron, $jobs, $defaults)
+
+  }
+
+}
