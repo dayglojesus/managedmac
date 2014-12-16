6ffa808f7527d5c094f805ad74f7681c94d099f6
Brian Warsing <bcw@sfu.ca>
2014-12-16 12:12:03 -0800

    fix issue #57 filevault_active - this fact was not working properly - something to do with popen3

--- lib/facter/filevault_active.rb
+++ lib/facter/filevault_active.rb
@@ -1,16 +1,8 @@
 require 'puppet'
-require 'open3'
 
 Facter.add("filevault_active") do
   confine :operatingsystem => :darwin
   setcode do
-    cmd = '/usr/bin/fdesetup isactive'
-    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-      case wait_thr.value.exitstatus
-      when 0, 2
-        return true
-      end
-      false
-    end
+    %x{/usr/bin/fdesetup isactive}.chomp.eql?('true')
   end
 end
