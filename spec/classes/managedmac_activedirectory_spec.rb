f6fdd9588e648866a0db3cdc9802c1079c8e49bb
Brian Warsing <bcw@sfu.ca>
2015-01-16 16:34:29 -0800

    re-spec to implement parameter: $evaluate

--- spec/classes/managedmac_activedirectory_spec.rb
+++ spec/classes/managedmac_activedirectory_spec.rb
@@ -15,7 +15,31 @@ describe 'managedmac::activedirectory', :type => 'class' do
     end
   end
 
-  context "when $enable == true but REQUIRED params params are NOT set" do
+  context "when $enable == false and $evaluate == 'false'" do
+    let(:params) do
+      {
+        :enable   => false,
+        :evaluate => 'false',
+      }
+    end
+    specify do
+      should_not contain_mobileconfig('managedmac.activedirectory.alacarte')
+    end
+  end
+
+  context "when $enable == false and $evaluate == 'no'" do
+    let(:params) do
+      {
+        :enable   => false,
+        :evaluate => 'no',
+      }
+    end
+    specify do
+      should_not contain_mobileconfig('managedmac.activedirectory.alacarte')
+    end
+  end
+
+  context "when $enable == true but REQUIRED params are NOT set" do
     let(:params) do
       { :enable => true }
     end
@@ -37,6 +61,98 @@ describe 'managedmac::activedirectory', :type => 'class' do
     end
     specify do
       should contain_mobileconfig('managedmac.activedirectory.alacarte').with_ensure('present')
+    end
+  end
+
+  context "when $enable == true and $seatbelt is INVALID" do
+    let(:params) do
+      {
+        :enable    => true,
+        :hostname  => 'foo.ad.com',
+        :username  => 'account',
+        :password  => 'password',
+        :evaluate  =>  "whatever",
+      }
+    end
+    specify do
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /Parameter.*must be/)
+    end
+  end
+
+  context "when $enable == true and $evaluate == undef" do
+    let(:params) do
+      {
+        :enable    => true,
+        :hostname  => 'foo.ad.com',
+        :username  => 'account',
+        :password  => 'password',
+        :evaluate  => '',
+      }
+    end
+    specify do
+      should contain_mobileconfig('managedmac.activedirectory.alacarte').with_ensure('present')
+    end
+  end
+
+  context "when $enable == true and $evaluate == 'true'" do
+    let(:params) do
+      {
+        :enable    => true,
+        :hostname  => 'foo.ad.com',
+        :username  => 'account',
+        :password  => 'password',
+        :evaluate  => 'true',
+      }
+    end
+    specify do
+      should contain_mobileconfig('managedmac.activedirectory.alacarte').with_ensure('present')
+    end
+  end
+
+  context "when $enable == true and $evaluate == 'yes'" do
+    let(:params) do
+      {
+        :enable    => true,
+        :hostname  => 'foo.ad.com',
+        :username  => 'account',
+        :password  => 'password',
+        :evaluate  => 'yes',
+      }
+    end
+    specify do
+      should contain_mobileconfig('managedmac.activedirectory.alacarte').with_ensure('present')
+    end
+  end
+
+  context "when $enable == true and $evaluate == 'no'" do
+    let(:params) do
+      {
+        :enable    => true,
+        :hostname  => 'foo.ad.com',
+        :username  => 'account',
+        :password  => 'password',
+        :evaluate  => 'no',
+      }
+    end
+    specify do
+      should_not contain_mobileconfig('managedmac.activedirectory.alacarte')
+    end
+  end
+
+  context "when $enable == true and $evaluate == 'false'" do
+    let(:params) do
+      {
+        :enable    => true,
+        :hostname  => 'foo.ad.com',
+        :username  => 'account',
+        :password  => 'password',
+        :evaluate  => 'false',
+      }
+    end
+    specify do
+      should_not contain_mobileconfig('managedmac.activedirectory.alacarte')
     end
   end
 
