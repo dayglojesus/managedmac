2e053943a9cb1328b5c466e0a5a37b79a00d90ec
Brian Warsing <bcw@sfu.ca>
2015-02-26 15:04:27 -0800

    new function for preprocessing dsconfigad params

---
+++ lib/puppet/parser/functions/process_dsconfigad_params.rb
@@ -0,0 +1,39 @@
+module Puppet::Parser::Functions
+  newfunction(:process_dsconfigad_params, :type => :rvalue, :doc => <<-EOS
+Returns a Hash for Dsconfigad type suitable for consumption by
+`create_resources`. It will compact the data and transform Booleans into the
+preferred enable|disable toggle. Accepts a single argument, a Hash.
+    EOS
+  ) do |args|
+
+    if args.size != 1
+      e = "process_dsconfigad_params(): Wrong number of args: #{args.size} \
+for 1"
+      raise(Puppet::ParseError, e)
+    end
+
+    params = args[0]
+
+    unless params.is_a? Hash
+      e = "process_dsconfigad_params(): Wrong arg type! (#{params.class} \
+instead of Hash)"
+      raise(Puppet::ParseError, e)
+    end
+
+    params.inject({}) do |memo, (key,value)|
+      value = case value
+      when NilClass, :absent, :undef
+        nil
+      when TrueClass
+        'enable'
+      when FalseClass
+        'disable'
+      else
+        value
+      end
+      memo[key] = value unless value.nil?
+      memo
+    end
+
+  end
+end
